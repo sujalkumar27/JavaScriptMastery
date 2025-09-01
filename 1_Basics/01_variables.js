919b8bdb255407416ef1480e9e3841c36d483de8
sujalkumar27 <93431670+sujalkumar27@users.noreply.github.com>
2025-09-01 11:43:07 +0000

    variable and datatypes

---
+++ 1_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const id="12345";
+
+let name="John Cena";
+
+var email="abc@gmail.com";//prefer not to use var due to issue in block scope and function scope
+
+console.log("ID:", id);
+
+city="New York"; // Implicit global variable (not recommended)
+
+let age; // Declared but not initialized
+
+// id="67890"; // Reassigning of constant variable not allowed
+
+console.table([ id ,name, email, city, age]);
---
+++ 1_Basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";// means that the code uses updated features of JavaScript
+
+// Datatypes
+// 1. Primitive datatypes: number, string, boolean, null, undefined, symbol, bigint
+// 2. Non-primitive datatypes: object (array, function, object)
+
+let name="Sujal"; // string 
+let age=20; // number   size 2 to the power of 53
+let isApproved=true; // boolean
+let firstName=null; // null
+let lastName=undefined; // undefined
+let selectedColor; // undefined
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isApproved);
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof selectedColor);
+
+
