0e5a7caf96cceee4f2cad5cba96d97f571c605c1
sujalkumar27 <93431670+sujalkumar27@users.noreply.github.com>
2025-09-03 07:14:21 +0000

    comparisons

---
+++ 1_Basics/04_comparisons.js
@@ -0,0 +1,28 @@
+//  console.log(1>2);
+//  console.log(1<2);
+//  console.log(1>=2);
+//  console.log(1<=2);
+ 
+//  console.log(1==1);
+//  console.log(1===1);
+//  console.log(1!=2);
+//  console.log(1!==2);
+
+//  console.log("1"==1);  //true
+//  console.log(1|2);  //3
+//  console.log(1&2);  //0
+//  console.log(1^2);  //3
+//  console.log(~1);  //-2
+//  console.log(1<<2);  //4
+//  console.log(4>>2);  //1
+
+
+ console.log(null>0);
+ console.log(null==0);
+ console.log(null>=0);
+
+ console.log(undefined==0);
+ console.log(undefined===0);
+ console.log(undefined!=0);
+ console.log(undefined!==0);
+
